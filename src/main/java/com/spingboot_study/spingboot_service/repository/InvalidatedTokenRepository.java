2c4bb0a51ba73df52fee7bb7ebdcdeda09d3c5bf
baokhangdev <khang.nguyenluubao@dcwizard.net>
2025-09-15 10:26:33 +0700

    feat : add LogoutRequest, InvalidatedTokenRepository, InvalidatedToken class

---
+++ src/main/java/com/spingboot_study/spingboot_service/repository/InvalidatedTokenRepository.java
@@ -0,0 +1,7 @@
+package com.spingboot_study.spingboot_service.repository;
+
+import com.spingboot_study.spingboot_service.entity.InvalidatedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
+}
