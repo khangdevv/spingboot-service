f8077f0662a20c84d642653cc7b1a91edc067206
baokhangdev <khang.nguyenluubao@dcwizard.net>
2025-10-13 14:28:29 +0700

    test : add testcase, edit test properties to active with h2 database and add jacoco ro report coverage

--- src/main/java/com/spingboot_study/spingboot_service/entity/User.java
+++ src/main/java/com/spingboot_study/spingboot_service/entity/User.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID) //random id ngau nhien tranh trung lap
